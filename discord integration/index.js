dc6bbbc860bef38acb7653457f3d247fed57ee39
Christopher Boyd <chrisboyd1123@gmail.com>
2021-04-27 14:08:53 -0500

    Feat: discord bot index file and corresponding folder added to mvp project.

---
+++ discord integration/index.js
@@ -0,0 +1,36 @@
+//Import bot token.
+const { TOKEN } = require(`./botToken.js.js`);
+
+//Import DiscordJS Library
+const Discord = require('discord.js');
+
+/////////////////////////////////
+// INITIALIZATION VIA NODE.JS //
+///////////////////////////////
+
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+    //Set bot activity.
+client.on('ready', () => {
+    console.log('Ready');
+    client.user.setActivity(``);
+})
+
+////////////////////////////////
+// MAIN LOGIC VIA DISCORD.JS //
+//////////////////////////////
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong.')
+    }
+  })
+
+////////////////////////////////////
+// CONNECTING BOT TO DISCORD API //
+//////////////////////////////////
+
+client.login(TOKEN);
