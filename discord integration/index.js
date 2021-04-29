f78d44f7bc0e3b4afb9e2165dfced459337cbe0c
Christopher Boyd <chrisboyd1123@gmail.com>
2021-04-29 14:03:30 -0500

    Fix: Discord bot initialization/login refactored to incorporate environment variables in development environment.

--- discord integration/index.js
+++ discord integration/index.js
@@ -1,8 +1,9 @@
-//Import bot token.
-const { TOKEN } = require(`./botToken.js`);
+//Access bot token.
+require('dotenv').config({ path: `../.env`});
 
 //Import DiscordJS Library
 const Discord = require('discord.js');
+const EventEmitter = require('events');
 
 /////////////////////////////////
 // INITIALIZATION VIA NODE.JS //
@@ -23,14 +24,38 @@ client.on('ready', () => {
 // MAIN LOGIC VIA DISCORD.JS //
 //////////////////////////////
 
+//PSEUDOCODE START//
+
+//When a user joins the "Team Player" server, give them their key to
+//a corresponding player profile via DMs.
+
+//When a user is "contacted" via the web-app, send them a DM over
+//Discord to accept the contact request.
+
+//Contact accepted? Create the following for both users.
+// - category for both users.
+// - text channel in category.
+// - voice chat in category.
+
+///////////////////
+
 client.on('message', msg => {
     if (msg.content === 'ping') {
-      msg.reply('pong.')
+      console.log("cache get user", client.users.cache.get('299722286947368974'));
     }
   })
+
+  client.on('test', () => {
+    console.log("user cache", client.users);
+    client.users.fetch(`299722286947368974`).then((result) => {
+     // result.send('lol4');
+    })
+  })
+
+  client.emit('test');
 
 ////////////////////////////////////
 // CONNECTING BOT TO DISCORD API //
 //////////////////////////////////
 
-client.login(TOKEN);
+client.login(process.env.BOT_TOKEN);
