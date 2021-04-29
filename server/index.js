485b33d3e8bad8534481d6d8fe4411cb0939c54c
Christopher Boyd <chrisboyd1123@gmail.com>
2021-04-29 15:15:01 -0500

    Feat: ExpressJS server instantiated.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 // TODO
+const ReactDOM = require('react-dom');
+const express = require("express");
+const { routes } = require('./router.js');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
