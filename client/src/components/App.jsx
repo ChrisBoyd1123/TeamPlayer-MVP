77031e358f6c5de3b030f7bc54bb26065fec8ae2
Christopher Boyd <chrisboyd1123@gmail.com>
2021-04-29 15:13:45 -0500

    Feat: App class component instantiated.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,1 +1,12 @@
 // TODO
+const React = require('react');
+
+export default class App extends React.Component{
+  render() {
+    return (
+      <div className="app">
+        lol
+      </div>
+    );
+  }
+}
