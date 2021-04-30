23fb03b13991838f52f9b0feaca35f58e7ff0258
Christopher Boyd <chrisboyd1123@gmail.com>
2021-04-30 16:33:48 -0500

    Feat: JSX file extensions supported, and HTMLPlugin removed in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
 // TODO
+const path = require('path');
+
+module.exports = {
+  entry: "./client/src/index",
+  output: { // NEW
+    path: path.join(__dirname, 'client/dist'),
+    filename: "bundle.js"
+  }, // NEW Ends
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  }
+};
