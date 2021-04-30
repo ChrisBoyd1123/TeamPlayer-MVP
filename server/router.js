8e9bc10ff4c7325279fab4c1fe3709c6fad203b9
Christopher Boyd <chrisboyd1123@gmail.com>
2021-04-30 16:32:51 -0500

    Feat: Route for serving webpack-compiled Bundle.js file now supported.

--- server/router.js
+++ server/router.js
@@ -1,7 +1,27 @@
+//Import ExpressJS server and routing functionality.
 const express = require("express");
 const router = express.Router();
+const fs = require('fs');
+
+//Import ReactDOM.
+const ReactDOM = require("react-dom");
+const path = require('path');
 
 router.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile(path.join(__dirname, '../client/public/index.html'), 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.write(data);
+    res.end();
+  })
+})
+
+router.get('/dist/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/bundle.js'));
+  res.end();
 })
 
 module.exports.routes = router;
