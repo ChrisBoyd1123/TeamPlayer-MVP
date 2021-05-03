912af52094457f0dd6e4bdc90ca8fc140f179eca
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-03 15:12:45 -0500

    Fix: File extension added for file pathway to App component.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 // TODO
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
