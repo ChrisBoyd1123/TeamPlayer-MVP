2d44afc93ae4ff0071b52b878318efcc49ad7435
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-03 15:13:13 -0500

    Feat: Static asset serving from dist folder now facilitated.

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ const { routes } = require('./router.js');
 const path = require('path');
 
 const PORT = process.env.PORT || 8080;
+const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
 
 const app = express();
 
+
+app.use(express.static(CLIENT_PATH));
 app.use('/', routes);
 
 app.listen(PORT, () => {
