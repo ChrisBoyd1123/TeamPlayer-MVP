7513bcafd64656d3ab219439f61c626699d94055
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-04 14:24:43 -0500

    Feat: Index.jsx now renders pages based on served HTML.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,14 @@
 // TODO
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import SignIn from './components/SignIn.jsx';
+import Profile from './components/Profile.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const signInRoot = document.getElementById('root-signin')
+const profileRoot = document.getElementById('root-profile')
+
+if(signInRoot){
+  ReactDOM.render(<SignIn />, signInRoot);
+}else if(profileRoot){
+  ReactDOM.render(<Profile />, profileRoot);
+}
