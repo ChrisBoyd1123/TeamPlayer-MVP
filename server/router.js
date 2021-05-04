6d5da8ef6123573bd9dfe77168e2ceb251504dc4
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-04 17:33:08 -0500

    Feat: User sign-in route and functionality established.

--- server/router.js
+++ server/router.js
@@ -8,6 +8,7 @@ const path = require('path');
 
 //Import database interaction helper functions.
 const { findUser } = require('../server/db/helpers.js');
+const { compareHash } = require('../server/auth/authUtils.js');
 
 router.get('/', (req, res) => {
   const { session } = req.cookies;
@@ -46,6 +47,32 @@ router.get('/playerProfile', (req, res) => {
     }
     res.write(data);
     res.end();
+  })
+})
+
+router.post('/signingIn', (req, res) => {
+  const usernamePattern = /^.*#[0-9]{4}$/;
+  //Test for "username#discriminator" pattern.
+  const { username, key } = req.body;
+
+  if(!usernamePattern.test(username)){
+    res.sendStatus(400).end();
+  }
+
+  const userAndDis = username.split("#");
+
+  findUser({username: userAndDis[0], discriminator: userAndDis[1]})
+  .then((dataArr) => {
+    if(!dataArr || !dataArr.length){
+      res.redirect('/signIn');
+    }else{
+      if(compareHash(key, dataArr[0].salt, dataArr[0].hash)){
+        res.cookie('session', dataArr[0].session);
+        res.redirect('/playerProfile');
+      }else{
+        res.redirect('/signIn');
+      }
+    }
   })
 })
 
