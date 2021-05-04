fdda76ffc0b7012221c56632e7f0869cc12565cc
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-04 14:27:10 -0500

    Feat: User sign in and profile JSX files established.

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+// TODO
+const React = require('react');
+
+export default class App2 extends React.Component{
+  render() {
+    return (
+      <div className="profile">
+        lol2
+      </div>
+    );
+  }
+}
---
+++ client/src/components/SignIn.jsx
@@ -0,0 +1,12 @@
+// TODO
+const React = require('react');
+
+export default class App extends React.Component{
+  render() {
+    return (
+      <div className="signin">
+        lol
+      </div>
+    );
+  }
+}
