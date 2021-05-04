16d66ffcde10462950c07232c54039d2c30fd644
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-04 18:00:07 -0500

    Feat: React SignIn component now facilitates user session-assigning functionality.

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,11 +1,48 @@
 // TODO
 const React = require('react');
+const axios = require('axios');
 
-export default class App extends React.Component{
+export default class SignIn extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    const submittedUser = document.getElementById("usernameInput").value;
+    const submittedKey = document.getElementById("keyInput").value;
+
+    const data = JSON.stringify(
+      {"username":submittedUser,
+      "key":submittedKey});
+    
+    const config = {
+      method: 'post',
+      url: 'http://127.0.0.1:8080/signingIn',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    //TODO: Find way to reload page after acquiring session from
+    //server /signingIn route.
+    axios(config)
+    .catch(function (err) {
+      console.log(err);
+    });
+  }
+
   render() {
     return (
       <div className="signin">
-        lol
+        <div>lol</div>
+        <div>
+        <input type="text" id='usernameInput'></input>
+        <input type="text" id='keyInput'></input>
+        <button onClick={this.handleSubmit} id='submitButton'>Enter</button>
+        </div>
       </div>
     );
   }
