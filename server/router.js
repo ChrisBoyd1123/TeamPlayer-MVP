1f061f48a1f07315e6b9315b8b987bb61ccd8326
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-04 15:25:47 -0500

    Feat: User session check, and applicable route redirection, established.

--- server/router.js
+++ server/router.js
@@ -6,7 +6,26 @@ const fs = require('fs');
 //Import Node path module.
 const path = require('path');
 
+//Import database interaction helper functions.
+const { findUser } = require('../server/db/helpers.js');
+
 router.get('/', (req, res) => {
+  const { session } = req.cookies;
+  if(!session){
+    res.redirect('/signIn');
+  }else{
+    findUser({ session: session })
+    .then((dataArr) => {
+      if(!dataArr || !dataArr.length){
+        res.redirect('/signIn');
+      }else{
+        res.redirect('/playerProfile');
+      }
+    })
+  }
+})
+
+router.get('/signIn', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   fs.readFile(path.join(__dirname, '../client/dist/signIn.html'), 'utf8' , (err, data) => {
     if (err) {
@@ -18,7 +37,7 @@ router.get('/', (req, res) => {
   })
 })
 
-router.get('/2', (req, res) => {
+router.get('/playerProfile', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   fs.readFile(path.join(__dirname, '../client/dist/profile.html'), 'utf8' , (err, data) => {
     if (err) {
