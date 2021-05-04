3a5e7da5fd7ce2438386d1c708bafa8259883f37
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-03 22:26:26 -0500

    Feat: App server now initializes after Mongo database.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 // TODO
 const ReactDOM = require('react-dom');
 const express = require("express");
+const path = require('path');
+
 const { routes } = require('./router.js');
 const { BOT_CLIENT } = require('../discord/index.js');
-const path = require('path');
+const { db } = require('./db/index.js');
 
 //Access bot token.
 require('dotenv').config();
@@ -17,7 +19,9 @@ const app = express();
 app.use(express.static(CLIENT_PATH));
 app.use('/', routes);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-  BOT_CLIENT.login(process.env.BOT_TOKEN);
-});
+db().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+    BOT_CLIENT.login(process.env.BOT_TOKEN);
+  });
+})
