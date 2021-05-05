3efac32b8f6d217902b72aed39314b103c7ceb39
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-05 13:36:58 -0500

    Feat: Profile component now utilizes js-cookie get method to access browser cookies.

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,11 +1,23 @@
 // TODO
 const React = require('react');
+const Cookies = require('js-cookie');
+
+//utilize local server to find user data
+//by saved session.
 
 export default class Profile extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      session: Cookies.get('session')
+    }
+  }
+
   render() {
     return (
       <div className="profile">
-        lol2
+        {this.state.session}
       </div>
     );
   }
