3f5577e3dbed50f895238a8bb7c66bb0422d3b15
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-05 23:58:57 -0500

    Feat: SQL Table models imported, and TODO pseudocode added.

--- mySQLRefactor/helpers.js
+++ mySQLRefactor/helpers.js
@@ -1,5 +1,6 @@
 //Functions dealing with database manipulation, such as adding, deleting,
 //finding, or updating users, should be included below:
+const { User, Hash, Salt, Session, Game, UserGame } = require('./initSequelize');
 
 module.exports.createUser = (userData) => {
   //userData should be an object with the following keys:
@@ -16,6 +17,10 @@ module.exports.createUser = (userData) => {
   })
 }
 
+//TODO: A general "findUser" function is likely not very conducive to a
+//relational SQL database environment.
+
+//Make multiple "find" helper functions based on what is needed by the web app.
 module.exports.findUser = (criteria) => {
   return new Promise((resolve, reject) => {
     
