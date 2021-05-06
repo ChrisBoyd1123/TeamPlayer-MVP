f73b75832036ec36487dd9aa3f55150d164c3561
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-06 00:01:58 -0500

    Fix: Previous SQL initializeSchema file refactored into two files - initSequelize and initSchema.

--- mySQLRefactor/initializeSchema.js
+++
@@ -1,8 +0,0 @@
-//TODO: Define appropriate table models, and establish table
-//relationships through sequelize.
-const { sequelize, Model, DataTypes} = require('./index.js');
-
-module.exports.initializeSchema = () => {
-  //Should return an object with keys and values corresponding to each
-  //created table model.
-}
