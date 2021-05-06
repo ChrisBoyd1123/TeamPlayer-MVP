dd33180f3b2244c7be5ab721280bc0baa985e7e0
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-06 00:00:49 -0500

    Feat: initSchema file established for Sequelize model syncing purposes.

---
+++ mySQLRefactor/initSchema.js
@@ -0,0 +1,28 @@
+//TODO
+const { sequelize, User, Hash, Salt, Session, Game, UserGame } = require('./initSequelize');
+const { DataTypes } = require('sequelize');
+const { STRING, INTEGER } = DataTypes;
+
+module.exports.initializeSchema = async () => {
+  /*
+  const query = sequelize.getQueryInterface();
+
+  query.createTable('Hashes', {
+    hash: STRING
+  })
+  */
+
+  return new Promise ((resolve, reject) => {
+    User.belongsTo(Hash);
+    User.belongsTo(Salt);
+    User.belongsTo(Session);
+
+    UserGame.belongsTo(User);
+    UserGame.belongsTo(Game);
+
+  sequelize.sync({force: true})
+    .then(() => {
+      resolve();
+    })
+  })
+}
