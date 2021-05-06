e820767c6815ffd6dc3088057258e5a058fc7400
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-06 13:13:28 -0500

    Feat: initSchema file for SQL database, using Sequelize, established.

---
+++ server/db/initSchema.js
@@ -0,0 +1,20 @@
+//TODO
+const { sequelize, User, Hash, Salt, Session, Game, UserGame } = require('./initSequelize');
+const { DataTypes } = require('sequelize');
+
+module.exports.initializeSchema = async () => {
+
+  return new Promise ((resolve, reject) => {
+    User.belongsTo(Hash);
+    User.belongsTo(Salt);
+    User.belongsTo(Session);
+
+    UserGame.belongsTo(User);
+    UserGame.belongsTo(Game);
+
+  sequelize.sync()
+    .then(() => {
+      resolve();
+    })
+  })
+}
