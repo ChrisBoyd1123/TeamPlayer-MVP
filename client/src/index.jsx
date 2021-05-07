43e14e185965ed3b0c0c678034d400c82485e313
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-06 22:30:44 -0500

    Feat: React index file now renders listing component when appropriate page is served.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SignIn from './components/SignIn.jsx';
 import Profile from './components/Profile.jsx';
+import Listing from './components/Listing.jsx';
 
 const signInRoot = document.getElementById('root-signin')
 const profileRoot = document.getElementById('root-profile')
+const listingRoot = document.getElementById('root-listing')
 
 if(signInRoot){
   ReactDOM.render(<SignIn />, signInRoot);
 }else if(profileRoot){
   ReactDOM.render(<Profile />, profileRoot);
+}else if(listingRoot){
+  ReactDOM.render(<Listing />, listingRoot);
 }
