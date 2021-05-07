fa45cd055d9685eae139a7b389e057e309f63514
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-07 06:36:52 -0500

    Feat: React Listing component now dynamically creates Listelements.

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -1,4 +1,6 @@
 // TODO
+import Listelement from './Listelement.jsx';
+
 const React = require('react');
 const Cookies = require('js-cookie');
 const axios = require('axios');
@@ -8,8 +10,46 @@ export default class Listing extends React.Component{
     super(props);
 
     this.state = {
-      session: Cookies.get('session')
+      session: Cookies.get('session'),
+      users: ['null|null|null'],
     }
+
+    this.handleUWSGLoad = this.handleUWSGLoad.bind(this);
+
+    this.handleUWSGLoad();
+  }
+
+  async handleUWSGLoad() {
+
+    const config = {
+      method: 'get',
+      url: `/usersWithSimilarGames`
+    };
+
+    const { data } = await axios(config)
+
+    console.log('Possible returned UWSG', data.UWSG);
+
+    if(data.UWSG){
+      let foundUsernames = [];
+      let foundDiscriminators = [];
+      let foundUserIds = [];
+
+      let foundUsers = [];
+
+      data.UWSG.forEach((userObj, uOIndex) => {
+        foundUsernames.push(userObj.username);
+        foundDiscriminators.push(userObj.discriminator);
+        foundUserIds.push(userObj.userId);
+      })
+
+      foundUsernames.forEach((username, uIndex) => {
+        foundUsers.push(`${username}|${foundDiscriminators[uIndex]}|${foundUserIds[uIndex]}`);
+      })
+
+      this.setState({users: foundUsers});
+    }
+
   }
 
   handleProfileLoad() {
@@ -20,6 +60,7 @@ export default class Listing extends React.Component{
     return (
       <div className="listing">
         <div><button onClick={this.handleProfileLoad}>Your Profile</button></div>
+        {this.state.users.map((item) => { return <Listelement username={item.split('|')[0]} discriminator={item.split('|')[1]} userId={item.split('|')[2]}></Listelement>})}
       </div>
     );
   }
