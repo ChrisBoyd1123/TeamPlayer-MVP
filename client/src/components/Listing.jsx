823e0cd2604afae7dbf822cd8a10a4ac7345eafc
Christopher Boyd <chrisboyd1123@gmail.com>
2021-05-10 16:22:53 -0500

    Feat: React Listing component now provisions a search bar, and facilitates user searches by game name.

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -15,6 +15,7 @@ export default class Listing extends React.Component{
     }
 
     this.handleUWSGLoad = this.handleUWSGLoad.bind(this);
+    this.handleGameSearch = this.handleGameSearch.bind(this);
 
     this.handleUWSGLoad();
   }
@@ -28,9 +29,7 @@ export default class Listing extends React.Component{
 
     const { data } = await axios(config)
 
-    console.log('Possible returned UWSG', data.UWSG);
-
-    if(data.UWSG){
+    if(data.UWSG && data.UWSG.length){
       let foundUsernames = [];
       let foundDiscriminators = [];
       let foundUserIds = [];
@@ -56,10 +55,58 @@ export default class Listing extends React.Component{
     window.location.replace(`/playerProfile`)
   }
 
+  async handleGameSearch() {
+    //Should send a POST request to app server with a game name,
+    //before receiving an array of user objects corresponding
+    //to the users that have the same game saved.
+
+    const searchedGame = document.getElementById('search-bar').value;
+
+    if(searchedGame){
+      const sendData = JSON.stringify(
+        {"gameName":searchedGame});
+      
+      const config = {
+        method: 'post',
+        url: `/usersWithSearchedGame`,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data : sendData
+      };
+
+      let { data } = await axios(config)
+
+      if(data.UWG && data.UWG.length){
+        let foundUsernames = [];
+        let foundDiscriminators = [];
+        let foundUserIds = [];
+  
+        let foundUsers = [];
+  
+        data.UWG.forEach((userObj, uOIndex) => {
+          foundUsernames.push(userObj.username);
+          foundDiscriminators.push(userObj.discriminator);
+          foundUserIds.push(userObj.userId);
+        })
+  
+        foundUsernames.forEach((username, uIndex) => {
+          foundUsers.push(`${username}|${foundDiscriminators[uIndex]}|${foundUserIds[uIndex]}`);
+        })
+  
+        this.setState({users: foundUsers});
+      }
+    }
+  }
+
   render() {
     return (
       <div className="listing">
         <div><button onClick={this.handleProfileLoad}>Your Profile</button></div>
+        <div>
+        <input id="search-bar"></input>
+        <button id="search-enter" onClick={this.handleGameSearch}>Enter</button>
+        </div>
         {this.state.users.map((item) => { return <Listelement username={item.split('|')[0]} discriminator={item.split('|')[1]} userId={item.split('|')[2]}></Listelement>})}
       </div>
     );
